bc481406c8d5a4afa7e9d647d5901597eb0ce008
AydinTuna <aydinbahadirtuna@gmail.com>
2023-11-19 14:38:24 +0300

    task3: proposal created

--- contracts/proposalContract.sol
+++ contracts/proposalContract.sol
@@ -1,7 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 contract ProposalContract {
+    using Counters for Counters.Counter;
+    Counters.Counter private _counter;
+
     struct Proposal {
         string title;
         string description; 
@@ -14,4 +19,9 @@ contract ProposalContract {
         bool isActive; // Can vote or cannot vote
     }
     mapping(uint256 => Proposal) proposalHistory; // Recordings of previous proposals
+
+    function create (string memory _title, string calldata _description, uint256 _totalVoteToEnd) external {
+        _counter.increment();
+        proposalHistory[_counter.current()] = Proposal(_title, _description, 0, 0, 0, _totalVoteToEnd, false, true);
+    }
 }
